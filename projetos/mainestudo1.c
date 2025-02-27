28f33c8f2256f1c20344125dc5721a88d1258c70
pmedeiross <patrickmedeiros2207@gmail.com>
2025-02-27 01:38:58 -0300

    first commit

---
+++ projetos/mainestudo1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   
+        int a, b, soma, subtracao, multiplicacao, divisao;
+
+        printf("***************** Bom dia *****************Hoje foi minha primeira aula de Prática de Programação!        *****************************************\n");
+        printf("Digite os dois valores:\n");
+        scanf("%d %d", &a, &b);
+        soma = a + b;
+        printf("soma eh: %d\n", soma);
+        subtracao = a - b;
+        printf("subtracao eh: %d\n", subtracao);
+        multiplicacao = a * b;
+        printf("multiplicacao eh: %d\n", multiplicacao);
+        divisao = a / b;
+        printf("divisao eh: %d\n", divisao);
+        
+
+
+    return 0;
+}
